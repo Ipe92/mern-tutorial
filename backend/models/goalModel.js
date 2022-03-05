38a8d554f000fc5c25e07014fc8757fe878880ba
ipe <iippol@protonmail.com>
2022-03-05 22:23:46 +0200

    Starting to add user stuff

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,12 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const goalSchema = mongoose.Schema({
-  text: {
-    type: String,
-    required: [true, 'Please add a text value']
-  }
-}, {
-  timestaps: true,
-})
+const goalSchema = mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			require: true,
+			ref: "User",
+		},
+		text: {
+			type: String,
+			required: [true, "Please add a text value"],
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
 
-module.exports = mongoose.model('Goal', goalSchema)
+module.exports = mongoose.model("Goal", goalSchema);
